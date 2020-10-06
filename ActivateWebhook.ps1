aa3349baa923739bdaf5903d84d052603ad22051
sweetserendpity <72474549+sweetserendpity@users.noreply.github.com>
2020-10-06 17:33:26 -0500

    Update ActivateWebhook.ps1

--- ActivateWebhook.ps1
+++ ActivateWebhook.ps1
@@ -1,8 +1,8 @@
-$description = "TrelBot"
-$callbackURL = "http://myserver:6969"
-$idmodel = "id of the trello board to connect to"
-$MemberToken = "ID of token"
-$APIkey = "API Key"
+$description = "Media & Publicity"
+$callbackURL = "https://discord.com/channels/758999222682517534/759000754798723148"
+$idmodel = "5f7cbe39d44525521be6512e"
+$MemberToken = "A0f7b18ce5207c2d025e9835648664668dbac7391d47a30cac94ab20b18f3a0d"
+$APIkey = "388b7cf9181fe08fb20ae61bd1a21b41"
 
 $postParams = @{description=$description;callbackURL=$callbackURL;idModel=$idmodel}
 $uri = "https://api.trello.com/1/tokens/" + $MemberToken + "/webhooks/?key=" + $APIkey
